f8969a95f0452e3758443bcfda4d0e2371d76e9e
Josiah Sayers <josiah.sayers15@gmail.com>
2026-06-21 19:45:57 -0400

    try to get artifacts to upload

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? [["github"], ["html", { open: "never" }]] : "html",
   use: {
     baseURL: process.env.BASE_URL ?? "http://localhost:3000",
     trace: "on-first-retry",
