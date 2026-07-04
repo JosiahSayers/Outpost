01489f6da83b5ef02eaf350ed0b657461e8cabc5
Josiah Sayers <47199245+JosiahSayers@users.noreply.github.com>
2026-07-04 14:19:21 -0400

    add a create endpoint for trips (#24)

--- app/routers/api/trip.ts
+++ app/routers/api/trip.ts
@@ -1,7 +1,7 @@
 import { requireValidSession } from "$/middleware/require-valid-session";
 import { transformers } from "$/transformers";
 import { db } from "$/utils/db";
-import { tripSearch } from "$/validation/trip";
+import { newTrip, tripSearch } from "$/validation/trip";
 import { Router } from "express";
 import validate from "express-zod-safe";
 
@@ -18,4 +18,20 @@ tripRouter.get("/", validate({ query: tripSearch }), async (req, res, next) => {
   });
 
   return res.json({ trips: trips.map(transformers.trip) });
+});
+
+tripRouter.post("/", validate({ body: newTrip }), async (req, res, next) => {
+  const newTrip = await db.trip.create({
+    data: {
+      name: req.body.name,
+      status: req.body.status,
+      trail: req.body.trail,
+      location: req.body.location,
+      start: req.body.start,
+      end: req.body.end,
+      userId: req.session!.user.id,
+    },
+  });
+
+  return res.status(201).json({ trip: transformers.trip(newTrip) });
 });
