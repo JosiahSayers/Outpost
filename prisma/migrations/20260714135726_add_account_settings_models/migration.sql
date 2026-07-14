854236751e6f6e50f38fba584a39492c75b1bf67
Josiah Sayers <josiah.sayers15@gmail.com>
2026-07-14 09:53:35 -0400

    add account setting models

---
+++ prisma/migrations/20260714135726_add_account_settings_models/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "AccountSetting" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "defaultValue" TEXT NOT NULL,
+
+    CONSTRAINT "AccountSetting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AccountSettingValue" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "value" TEXT NOT NULL,
+    "accountSettingId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "AccountSettingValue_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AccountSetting_name_key" ON "AccountSetting"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AccountSettingValue_accountSettingId_userId_key" ON "AccountSettingValue"("accountSettingId", "userId");
+
+-- AddForeignKey
+ALTER TABLE "AccountSettingValue" ADD CONSTRAINT "AccountSettingValue_accountSettingId_fkey" FOREIGN KEY ("accountSettingId") REFERENCES "AccountSetting"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AccountSettingValue" ADD CONSTRAINT "AccountSettingValue_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
