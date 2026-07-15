eef97cae5a1f40f649c777a0579e8b2a43d6dba1
Josiah Sayers <josiah.sayers15@gmail.com>
2026-07-14 21:28:16 -0400

    wire up email send into better-auth

--- app/jobs/queues.ts
+++ app/jobs/queues.ts
@@ -1,5 +1,8 @@
 import { redisConnection } from "$/jobs/workers/default-options";
-import { EMAILS__RESET_PASSWORD_WORKER } from "$/jobs/workers/email/reset-password";
+import {
+  EMAILS__RESET_PASSWORD_WORKER,
+  type SendResetPasswordEmailData,
+} from "$/jobs/workers/email/reset-password";
 import { TRIPS__MOVE_TO_FINISHED_WORKER } from "$/jobs/workers/trip-status/move-to-finished";
 import { TRIPS__MOVE_TO_IN_PROGRESS_WORKER } from "$/jobs/workers/trip-status/move-to-in-progress";
 import { Queue } from "bullmq";
@@ -13,12 +16,10 @@ export const moveToFinishedQueue = new Queue(TRIPS__MOVE_TO_FINISHED_WORKER, {
   connection: redisConnection,
 });
 
-export const sendResetPasswordEmailQueue = new Queue(
-  EMAILS__RESET_PASSWORD_WORKER,
-  {
+export const sendResetPasswordEmailQueue =
+  new Queue<SendResetPasswordEmailData>(EMAILS__RESET_PASSWORD_WORKER, {
     connection: redisConnection,
-  },
-);
+  });
 
 export const allQueues = [
   moveToInProgressQueue,
