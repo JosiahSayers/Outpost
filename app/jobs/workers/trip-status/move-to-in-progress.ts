4472a33921f69e9a136c6c2cb75c7382d94070e7
Josiah Sayers <josiah.sayers15@gmail.com>
2026-07-31 15:34:37 -0400

    implement notification for trip moved to in progress

--- app/jobs/workers/trip-status/move-to-in-progress.ts
+++ app/jobs/workers/trip-status/move-to-in-progress.ts
@@ -1,10 +1,39 @@
-import { defaultWorkerOptions } from "$/jobs/workers/default-options";
+import {
+  defaultWorkerOptions,
+  redisConnection,
+} from "$/jobs/workers/default-options";
+import {
+  NOTIFICATIONS__CREATE_NOTIFICATION,
+  type CreateNotificationJobData,
+} from "$/jobs/workers/notifications/create-notification";
 import { db } from "$/utils/db";
-import { Worker } from "bullmq";
+import { Queue, Worker } from "bullmq";
 
 export const TRIPS__MOVE_TO_IN_PROGRESS_WORKER = "trips__move_to_in_progress";
 
+const createNotificationQueue = new Queue<CreateNotificationJobData>(
+  NOTIFICATIONS__CREATE_NOTIFICATION,
+  {
+    connection: redisConnection,
+  },
+);
+
 const BATCH_SIZE = 1000;
+
+export const IN_PROGRESS_NOTIFICATION_TITLES = [
+  "Your trip has started!",
+  "Adventure time!",
+  "You're on the move",
+  "Trip's officially underway",
+  "Let the trip begin",
+];
+
+function randomInProgressNotificationTitle() {
+  const index = Math.floor(
+    Math.random() * IN_PROGRESS_NOTIFICATION_TITLES.length,
+  );
+  return IN_PROGRESS_NOTIFICATION_TITLES[index]!;
+}
 
 export async function moveTripsToInProgress(now: Date = new Date()) {
   const startOfDayUTC = new Date(
@@ -22,6 +51,7 @@ export async function moveTripsToInProgress(now: Date = new Date()) {
     const tripsToMove = await db.trip.findMany({
       select: {
         id: true,
+        userId: true,
       },
       where: {
         status: "planning",
@@ -40,6 +70,19 @@ export async function moveTripsToInProgress(now: Date = new Date()) {
     });
 
     changedTripIds.push(...tripIdsToMove);
+
+    await createNotificationQueue.addBulk(
+      tripsToMove.map(({ id, userId }) => ({
+        name: "trip-moved-to-in-progress-notification",
+        data: {
+          userId,
+          title: randomInProgressNotificationTitle(),
+          description: "We've automatically marked your trip as in progress.",
+          icon: "PersonSimpleHikeIcon",
+          referenceUrl: `/trips/${id}`,
+        },
+      })),
+    );
 
     if (tripsToMove.length === BATCH_SIZE) {
       await processBatch();
